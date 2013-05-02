ab20d57e89cb1720c4bb63b67d6fd570b606f231
Christopher Abad <aempirei@gmail.com>
2013-05-01 19:13:36 -0700

    .

--- pmore.c
+++ pmore.c
@@ -3,7 +3,8 @@
  * POST MODERN UNIX TOOLS
  * pmore
  *
- * ansi-color-safe more-like command
+ * ansi-code and other terminal escape sequence safe/preserving
+ * more-like command although does not accept pipe-style stdin mode
  *
  * Copyright(c) 2013 by Christopher Abad
  * aempirei@gmail.com
@@ -23,10 +24,22 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <termios.h>
 #include <libgen.h>
 #include "version.h"
 
-size_t scroll_sz = 80 * 4;
+size_t scroll_sz = 80 * 12;
+struct termios t, s;
+
+void exithandler() {
+    tcsetattr(0, TCSANOW, &s);
+}
+
+void sighandler(int signo) {
+
+    signal(signo, SIG_IGN);
+    exit(EXIT_SUCCESS);
+}
 
 const char *version() {
     static char string[80];
@@ -36,8 +49,8 @@ const char *version() {
 }
 
 void help(const char *prog) {
-    fprintf(stderr, "\nusage: %s [options] [filename]\n\n", prog);
-    fprintf(stderr, "press ENTER to output more data (currently %d bytes at a time)\n\n", (int)scroll_sz);
+    fprintf(stderr, "\nusage: %s [options] filename\n\n", prog);
+    fprintf(stderr, "press SPACEBAR or ENTER to scroll forward %d bytes and Q to quit\n\n", (int)scroll_sz);
     fputs("\t-b num   display num bytes at a time (valid for num > 0 obviously)\n", stderr);
     fputs("\t-h       show this help\n", stderr);
     fputs("\t-V       show version\n\n", stderr);
@@ -70,24 +83,63 @@ int main(int argc, char **argv) {
 
     fn = argv[optind];
 
-    if (scroll_sz < 1) {
+    if (fn == NULL || scroll_sz < 1) {
         help(basename(*argv));
         exit(EXIT_FAILURE);
     }
 
-    if(fn == NULL) {
-	fp = stdin;
-    } else if ((fp = fopen(fn, "r")) == NULL) {
+    if ((fp = fopen(fn, "r")) == NULL) {
         perror("fopen()");
         exit(EXIT_FAILURE);
     }
 
     buffer = malloc(scroll_sz + 1);
 
-    while ((n = fread(buffer, 1, scroll_sz, fp)) != 0 && !feof(fp)) {
-        fwrite(buffer, 1, n, stdout);
-        getchar();
+    /*
+     * save terminal state
+     */
+
+    if (tcgetattr(0, &t) == -1) {
+        perror("tcgetattr()");
+        exit(EXIT_FAILURE);
     }
+
+    s = t;
+
+    /*
+     * register the terminal cleanup exit handler
+     */
+
+    atexit(exithandler);
+
+    /*
+     * turn off echoing
+     */
+
+    t.c_lflag &= ~(ECHO | ICANON);
+    t.c_cc[VTIME] = 0;
+    t.c_cc[VMIN] = 1;
+    tcsetattr(0, TCSANOW, &t);
+
+    /*
+     * the only reason we're here! to get a single keystroke!
+     */
+
+    while ((n = fread(buffer, 1, scroll_sz, fp)) != 0 && !feof(fp)) {
+        int ch;
+
+        fwrite(buffer, 1, n, stdout);
+
+        do {
+            ch = getchar();
+            if (ch == 'q' || ch == 'Q') {
+                fseek(fp, 0, SEEK_END);
+                break;
+            }
+        } while (ch != ' ' && ch != '\n');
+    }
+
+    free(buffer);
 
     exit(EXIT_SUCCESS);
 }
