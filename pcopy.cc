bf5ed25162ae7c02a9eb8791d3e1b39510009dc8
Christopher Abad <aempirei@gmail.com>
2015-11-18 01:46:41 -0800

    .

--- pcopy.cc
+++ pcopy.cc
@@ -27,20 +27,6 @@
 
 #include "version.h"
 
-#define MIN(a,b) ((a)<(b)?(a):(b))
-
-int emsgi(const char *func, long n) {
-	int en = errno;
-	fprintf(stderr, "%s(%ld): %s\n", func, n, strerror(errno));
-	return en;
-}
-
-int emsg(const char *func, const char *str) {
-	int en = errno;
-	fprintf(stderr, "%s(%s): %s\n", func, str, strerror(errno));
-	return en;
-}
-
 const char *bytestr(size_t sz) {
 
 	static const int base = 1024;
@@ -68,32 +54,154 @@ const char *filesize(const char *fn) {
 	return bytestr(st.st_size);
 }
 
-void copy_file(const char *src, const char *dst) {
+void print_bar(long a, long b, int x) {
 
-	struct stat st;
-	int fdsrc, fddst;
+		static int y0 = -1;
 
-	fdsrc = open(src, O_RDONLY);
-	if(fdsrc == -1) {
-		emsg("open", src);
-	}
+		int y = x * a / b;
 
-	if(fstat(fdsrc, &st) == -1) {
-		emsg("fstat", src);
-		close(fdsrc);
-	}
+		if(y != y0) {
 
-	fddst = open(dst, O_WRONLY | O_CREAT | O_TRUNC, st.st_mode & 0777);
-	if(fddst == -1) {
-			emsg("open", dst);
-	}
+				fputs("\033[s", stdout);
 
-	if(fchmod(fddst, st.st_mode & 07777) == -1) {
-			emsg("fchmod", dst);
-	}
+				putchar('[');
 
-	close(fddst);
-	close(fdsrc);
+				for(int i = 0; i < y; i++)
+						putchar('#');
+
+				for(int i = y; i < x; i++)
+						putchar('.');
+
+				putchar(']');
+
+				fputs("\033[u", stdout);
+		}
+
+		y0 = y;
+}
+
+int write2(int fd, const void *buf, size_t buf_sz) {
+
+		ssize_t left, done, n;
+		const char *p;
+
+		p = (const char *)buf;
+		left = buf_sz;
+		done = 0;
+
+		while(left > 0) {
+				n = write(fd, p + done, left);
+
+				if(n == -1) {
+						if(errno == EINTR)
+								continue;
+						return -1;
+				}
+
+				left -= n;
+				done += n;
+		}
+
+		return 0;
+}
+
+int copy_block(int src, int dst, off_t offset, size_t block_sz, size_t chunk_sz) {
+
+		const int bar_sz = 64;
+
+		char *chunk = new char[chunk_sz];
+
+		ssize_t left, done, n;
+
+		bool success = false;
+
+		if(lseek(src, offset, SEEK_SET) == (off_t)-1)
+				goto cleanup;
+
+		done = 0;
+		left = block_sz;
+
+		while(left > 0) {
+				n = read(src, chunk, chunk_sz);
+				if(n == -1) {
+						if(errno == EINTR)
+								continue;
+						goto cleanup;
+				}
+
+				if(write2(dst, chunk, n) == -1)
+						goto cleanup;
+
+				left -= n;
+				done += n;
+
+				print_bar(done, block_sz, bar_sz);
+
+		}
+
+		success = true;
+
+cleanup:
+
+		fputs("\033[K", stdout);
+
+		delete[] chunk;
+
+		return success ? 0 : -1;
+}
+
+int copy_file(const char *src, const char *dst) {
+
+		const int chunk_sz = 1 << 16;
+
+		struct stat st;
+		int fdsrc = -1;
+		int fddst = -1;
+		bool success = false;
+
+		fputs("\033[?25l", stdout);
+
+		fdsrc = open(src, O_RDONLY);
+		if(fdsrc == -1)
+				goto cleanup;
+
+		if(fstat(fdsrc, &st) == -1)
+				goto cleanup;
+
+		fddst = open(dst, O_WRONLY | O_CREAT | O_TRUNC, st.st_mode & 0777);
+		if(fddst == -1)
+				goto cleanup;
+
+		if(fchmod(fddst, st.st_mode & 07777) == -1)
+				goto cleanup;
+
+		printf("%s -> %s :: ", src, dst);
+
+		if(copy_block(fdsrc, fddst, 0, st.st_size, chunk_sz) == -1)
+				goto cleanup;
+
+		success = true;
+
+cleanup:
+
+		int err = errno;
+
+		fputs("\033[?25h", stdout);
+
+		puts(success ? "done" : "error");
+
+		if(fdsrc >= 0)
+				close(fdsrc);
+
+		if(fddst >= 0) {
+				close(fddst);
+				if(not success)
+						unlink(dst);
+		}
+
+		errno = err;
+
+		return success ? 0 : -1;
 }
 
 const char * version() {   
@@ -144,7 +252,10 @@ int main(int argc, char **argv) {
 		return EXIT_FAILURE;
 	}
 
-	copy_file(argv[optind], argv[optind + 1]);
+	if(copy_file(argv[optind], argv[optind + 1]) == -1) {
+			perror("copy_file()");
+			return EXIT_FAILURE;
+	}
 
 	return EXIT_SUCCESS;
 }
