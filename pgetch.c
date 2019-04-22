dfbf4b30078809a4c5ccfec519fa0366336f34d7
Christopher Abad <christopher@abad.ml>
2019-04-22 08:36:37 -0700

    .

--- pgetch.c
+++ pgetch.c
@@ -88,52 +88,82 @@ const char *charmap(int ch) {
    return charmaps;
 }
 
-/*
- * escape sequence modifier combinations:
- *
- * 2: SHIFT
- * 3: ALT
- * 4: ALT+SHIFT
- * 5: CTRL
- * 6: CTRL+SHIFT
- * 7: CTRL+ALT
- * 8: CTRL+ALT+SHIFT
- *
- */
+const char *mod(unsigned int n) {
+	switch(n) {
+		case 2: return "SHIFT";
+		case 3: return "ALT";
+		case 4: return "ALT+SHIFT";
+		case 5: return "CTRL";
+		case 6: return "CTRL+SHIFT";
+		case 7: return "CTRL+ALT";
+		case 8: return "CTRL+ALT+SHIFT";
+		default: return NULL;
+	}
+}
+
+const char *esc(char f, int a, char c) {
+	if(f != '[' && f != 'O') {
+		return NULL;
+	} else if(c == '~') switch(a) {
+		case 1:return "HOME";
+		case 2:return "INSERT";
+		case 3:return "DELETE";
+		case 4:return "END";
+		case 5:return "PAGEUP";
+		case 6:return "PAGEDOWN";
+		case 11:return "F1";
+		case 12:return "F2";
+		case 13:return "F3";
+		case 14:return "F4";
+		case 15:return "F5";
+		case 16:return "F6";
+		case 18:return "F7";
+		case 19:return "F8";
+		case 20:return "F9";
+		case 21:return "F10";
+		case 23:return "F11";
+		case 24:return "F12";
+	} else switch(c) {
+		case 'A':return "UP";
+		case 'B':return "DOWN";
+		case 'C':return "RIGHT";
+		case 'D':return "LEFT";
+		case 'H':return "HOME";
+		case 'F':return "END";
+		case 'P':return "F1";
+		case 'Q':return "F2";
+		case 'R':return "F3";
+		case 'S':return "F4";
+		case 'Z': return "SHIFT+TAB";
+	}
+
+	return NULL;
+}
+
+const char *not_null(const char *a, const char *b) {
+	return a == NULL ? b : a;
+}
 
 const char *escmap(const char *s) {
    static char escmaps[40];
-#define SMAP(a,b) if(strcmp(a,s)==0) return(b)
-   SMAP("OP","NUMLOCK");
-   else SMAP("OH","HOME");
-   else SMAP("OF","END");
-   else SMAP("[P", "PAUSE");
-   else SMAP("[A", "UP");
-   else SMAP("[B", "DOWN");
-   else SMAP("[C", "RIGHT");
-   else SMAP("[D", "LEFT");
-   else SMAP("[1~","HOME");
-   else SMAP("[2~","INSERT");
-   else SMAP("[3~","DELETE");
-   else SMAP("[4~","END");
-   else SMAP("[5~","PAGEUP");
-   else SMAP("[6~","PAGEDOWN");
-   else SMAP("[11~","F1");
-   else SMAP("[12~","F2");
-   else SMAP("[13~","F3");
-   else SMAP("[14~","F4");
-   else SMAP("[15~","F5");
-   else SMAP("[16~","F6");
-   else SMAP("[17~","F7");
-   else SMAP("[18~","F8");
-   else SMAP("[20~","F9");
-   else SMAP("[21~","F10");
-   else SMAP("[23~","F11");
-   else SMAP("[24~","F12");
-   else SMAP("[Z","SHIFT-TAB");
-#undef SMAP
-   else snprintf(escmaps, sizeof(escmaps), "ESC+%s", s);
-   return escmaps;
+   unsigned short a, b;
+   char f, c;
+   const char *e, *m, *r;
+   snprintf(escmaps, sizeof(escmaps), "<ESC>%s", s);
+   r = escmaps;
+   f = *s++;
+   if(sscanf(s, "%hu;%hu%c", &a, &b, &c) == 3) {
+	   m = mod(b);
+	   e = esc(f,a,c);
+	   if(m != NULL && e != NULL)
+		   snprintf(escmaps, sizeof(escmaps), "%s+%s",m,e);
+   } else if(sscanf(s, "%hu%c", &a, &c) == 2) {
+	   r = not_null(esc(f,a,c), escmaps);
+   } else {
+	   c = *s++;
+	   r = not_null(esc(f,-1,c), escmaps);
+   }
+   return r;
 }
 
 int
